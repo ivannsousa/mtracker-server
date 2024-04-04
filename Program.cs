d54bcd158d80b25c43bf58ebfc4f6d30617b2477
Ivan Sousa <silvaivann14@gmail.com>
2024-04-04 17:31:45 -0300

    feat: configura client do mongodb no projeto

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+builder.Services.AddSingleton(_ => new MongoClient());
+builder.Services.AddSingleton(
+    provider => provider.GetRequiredService<MongoClient>().GetDatabase("mtracker"));
+builder.Services.AddSingleton(
+    provider => provider.GetRequiredService<IMongoDatabase>().GetCollection<Position>("tracker"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.MapHub<TrackerHub>("/tracker");
 app.Run();
